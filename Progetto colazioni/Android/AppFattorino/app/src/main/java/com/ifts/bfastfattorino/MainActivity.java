4ee8be95310da32450a6d67b0e4a96e1c775a999
guglielmo-strambini <57755173+guglielmo-strambini@users.noreply.github.com>
2020-01-22 17:39:12 +0100

    Schermata login android

--- Progetto colazioni/Android/AppFattorino/app/src/main/java/com/ifts/bfastfattorino/MainActivity.java
+++ Progetto colazioni/Android/AppFattorino/app/src/main/java/com/ifts/bfastfattorino/MainActivity.java
@@ -3,6 +3,8 @@ package com.ifts.bfastfattorino;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.textView2);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_registrazione);
+            }
+        });
     }
 }
