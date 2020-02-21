1e6d4cc15521575c61e3c6e9edff550e17ea450f
guglielmo-strambini <57755173+guglielmo-strambini@users.noreply.github.com>
2020-02-21 15:30:34 +0100

    Creazione controller per le pagine html degli ordini

--- Progetto colazioni/HTML-JAVA/b.fast/src/main/java/business/TotaleOrdiniGiornalieri.java
+++ Progetto colazioni/HTML-JAVA/b.fast/src/main/java/business/TotaleOrdiniGiornalieri.java
@@ -1,9 +1,12 @@
 package business;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
 import model.Bar;
+import model.Ordine;
 import utils.JPAUtil;
 
 public class TotaleOrdiniGiornalieri {
@@ -16,4 +19,13 @@ public class TotaleOrdiniGiornalieri {
 		return Ris.executeUpdate();
 		}
 
+	public List<Ordine> PagVisualizza(int id){
+		EntityManager em = JPAUtil.getInstance().getEmf().createEntityManager();
+		Bar b = em.find(Bar.class, id);
+		Query Ris = em.createQuery("SELECT COUNT(*) as conteggio FROM ordini as o, bar as b\r\n" + 
+				"WHERE DAY(o.data) = DAY(getdate()) and b.id = o.IDbarFK  o.Confermato = '1'").setParameter("b.id", b.getId());
+		List<Ordine> lista = Ris.getResultList(); 
+		return lista;
+	}
+	
 }
