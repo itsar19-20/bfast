c7100dccfa434e3d6586e906d53ed7818e0998e3
guglielmo-strambini <57755173+guglielmo-strambini@users.noreply.github.com>
2020-03-10 17:49:19 +0100

    Implementazione rest nell'app utente
    manca ancora retrofit2

---
+++ Progetto colazioni/Android/AppUtente/app/src/main/java/com/ifts/bfastutente/MyApiEndpointInterface.java
@@ -0,0 +1,65 @@
+package com.ifts.bfastutente;
+
+import com.ifts.bfastutente.ModelAPP.Utente;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface MyApiEndpointInterface {
+
+    @GET("/login")
+    Call<Utente> login(@Query("mail")String mail, @Query("password") String pwd);
+
+    @GET("/registrazione")
+    Call<Utente> registrazione(@Query("username")String usrnm, @Query("password") String pwd,@Query("username")String usrnm, @Query("password") String pwd);
+
+    @GET("/CancellazioneUtente")
+    Call<Utente> Cancellazione(@Query("mail")String mail, @Query("password") String pwd);
+
+    @GET("/ConfermaPosizione")
+
+
+    @GET("/ConfermaMail")
+    Call<Utente> ConfermaMail(@Query("mail")String mail);
+
+
+    @GET("/CambioMail")
+    Call<Utente> CambioMail(@Query("mail")String mail, @Query("mail") String mail);
+
+
+    @GET("/CambioPassword")
+    Call<Utente> CambioPassword(@Query("password")String pass, @Query("copassword") String copass);
+
+
+    @GET("/PasswordDimenticata")
+    Call<Utente> login(@Query("mail")String mail);
+
+
+    @GET("/Ordini")
+
+
+    @GET("/SelezioneBar")
+
+
+    @GET("/SelezionePosizione")
+
+
+    @GET("/SelezioneProdotti")
+
+
+    @GET("/ScriviDomanda")
+
+
+    @GET("/ScriviRisposta")
+
+
+    @GET("CercaDomanda")
+
+
+}
