b8e269cb48a56b1c69e34b239391adfcdd224147
guglielmo-strambini <57755173+guglielmo-strambini@users.noreply.github.com>
2020-03-11 12:55:50 +0100

    Fine implementazione model(interni) App fattorino

---
+++ Progetto colazioni/Android/AppFattorino/app/src/main/java/com/ifts/bfastfattorino/ModelAPP/PosizioneFattorino.java
@@ -0,0 +1,37 @@
+package com.ifts.bfastfattorino.ModelAPP;
+
+public class PosizioneFattorino {
+
+    private int id;
+
+    private double posXFA;
+
+    private double posYFA;
+
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPosXFA() {
+        return this.posXFA;
+    }
+
+    public void setPosXFA(double posXFA) {
+        this.posXFA = posXFA;
+    }
+
+    public double getPosYFA() {
+        return this.posYFA;
+    }
+
+    public void setPosYFA(double posYFA) {
+        this.posYFA = posYFA;
+    }
+
+
+}
