376d81e6707675e1c13c7dd04c8f55f73c80639d
guglielmo-strambini <57755173+guglielmo-strambini@users.noreply.github.com>
2020-03-11 09:54:00 +0100

    Aggiunta libreria Retrofit su tutti i progetti

--- Progetto colazioni/Android/AppFattorino/app/src/main/java/com/ifts/bfastfattorino/MyApiEndpointInterface.java
+++ Progetto colazioni/Android/AppFattorino/app/src/main/java/com/ifts/bfastfattorino/MyApiEndpointInterface.java
@@ -1,13 +1,10 @@
 package com.ifts.bfastfattorino;
 
-import java.util.List;
+import com.ifts.bfastfattorino.ModelAPP.Fattorino;
 
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
-import retrofit2.http.PUT;
-import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface MyApiEndpointInterface {
 
--- Progetto colazioni/Android/AppUtente/app/src/main/java/com/ifts/bfastutente/MyApiEndpointInterface.java
+++ Progetto colazioni/Android/AppUtente/app/src/main/java/com/ifts/bfastutente/MyApiEndpointInterface.java
@@ -2,14 +2,9 @@ package com.ifts.bfastutente;
 
 import com.ifts.bfastutente.ModelAPP.Utente;
 
-import java.util.List;
-
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
-import retrofit2.http.PUT;
-import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface MyApiEndpointInterface {
 
