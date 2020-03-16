8867ff27135b8848d04756e001194fd80597c496
Danidep <57755865+Danidep@users.noreply.github.com>
2020-03-16 17:07:08 +0100

    inizio ordine adapter

--- Progetto colazioni/Android/AppUtente/app/src/main/java/com/ifts/bfastutente/Adapter/DatabaseHelper.java
+++ Progetto colazioni/Android/AppUtente/app/src/main/java/com/ifts/bfastutente/Adapter/DatabaseHelper.java
@@ -9,6 +9,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final int DB_VERSION = 1;
     private static final String DB_CREATE = "create table Utente (email text primary key , nome text not null unique, " +
             "password text not null, cognome text not null, telefono int not null,nascita Date not null);";
+    private static final String DB_CREATE2 = "create table Ordine (id int primary key autoincremented, orario text not null unique, " +
+            "password text not null, cognome text not null, telefono int not null,nascita Date not null);";
 
     public DatabaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
--- Progetto colazioni/Android/AppUtente/app/src/main/java/com/ifts/bfastutente/Business/Ordini.java
+++ Progetto colazioni/Android/AppUtente/app/src/main/java/com/ifts/bfastutente/Business/Ordini.java
@@ -1,4 +1,48 @@
 package com.ifts.bfastutente.Business;
 
-public class Ordini {
-}
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ifts.bfastutente.Adapter.UserDBAdapter;
+import com.ifts.bfastutente.ModelAPP.Bar;
+import com.ifts.bfastutente.ModelAPP.Ordine;
+import com.ifts.bfastutente.ModelAPP.Utente;
+
+    public class Ordini extends AppCompatActivity {
+        UserDBAdapter udba = new UserDBAdapter(this);
+
+        public Ordine creazione(String idu) {
+            Ordine o = new Ordine();
+
+            Utente u = em.find(Utente.class, idu);
+            o.setUtente(u);
+            em.getTransaction().begin();
+            em.persist(o);
+            em.getTransaction().commit();
+            return o;
+        }
+
+        public Ordine bar(int ido,int idb) {
+            Ordine o = null;
+            EntityManager em = JPAUtil.getInstance().getEmf().createEntityManager();
+            o = em.find(Ordine.class, ido);
+            Bar b = em.find(Bar.class, idb);
+            em.getTransaction().begin();
+            o.setBar(b);
+            em.getTransaction().commit();
+            return o;
+        }
+
+        public Ordine carrello(int ido,String orario,String Note) {
+            Ordine o = null;
+            EntityManager em = JPAUtil.getInstance().getEmf().createEntityManager();
+            o = em.find(Ordine.class, ido);
+            em.getTransaction().begin();
+            o.setOrario(orario);
+            o.setNote(Note);
+            em.getTransaction().commit();
+            return o;
+        }
+
+
+    }
+
