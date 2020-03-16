817a7c59d46bd4281145dd63d95385170c686208
guglielmo-strambini <57755173+guglielmo-strambini@users.noreply.github.com>
2020-03-16 17:47:05 +0100

    Creazione password dimenticata utente

--- Progetto colazioni/Android/AppUtente/app/src/main/java/com/ifts/bfastutente/Business/pswDimenticata.java
+++ Progetto colazioni/Android/AppUtente/app/src/main/java/com/ifts/bfastutente/Business/pswDimenticata.java
@@ -1,4 +1,21 @@
 package com.ifts.bfastutente.Business;
 
-public class pswDimenticata {
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ifts.bfastutente.Adapter.UserDBAdapter;
+import com.ifts.bfastutente.ModelAPP.Utente;
+
+public class pswDimenticata extends AppCompatActivity {
+    public Utente cambio(int s, String password, String Copassword) {
+
+        Utente _return = null;
+        UserDBAdapter udba = new UserDBAdapter(this);
+        // cerca utente
+        if (password.equals(Copassword)) {
+            udba.open();
+            udba.updateUser(_return.getEmail(),password,_return.getNome(),_return.getCognome(),_return.getTelefono(),_return.getNascita());
+            udba.close();
+        }
+        return _return;
+    }
 }
