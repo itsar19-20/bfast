94fa99ca107275e95b922d502690c3d1c2ac891a
guglielmo-strambini <57755173+guglielmo-strambini@users.noreply.github.com>
2020-03-17 14:16:17 +0100

    Creazione adapter per model domanda app utente

--- Progetto colazioni/Android/AppUtente/app/src/main/java/com/ifts/bfastutente/Adapter/DatabaseHelper.java
+++ Progetto colazioni/Android/AppUtente/app/src/main/java/com/ifts/bfastutente/Adapter/DatabaseHelper.java
@@ -11,6 +11,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             "password text not null, cognome text not null, telefono int not null,nascita Date not null);";
     private static final String DB_CREATE2 = "create table Ordine (id int primary key autoincrement, orario text not null unique, " +
             "note text not null, data Date not null, confermato bit not null,valutazioneFatt float not null);";
+    private static final String DB_CREATE3 = "create table Domanda (id integer primary key autoincrement, testo text not null unique);";
+
 
     public DatabaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -19,12 +21,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(DB_CREATE);
         db.execSQL(DB_CREATE2);
+        db.execSQL(DB_CREATE3);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS Utente");
         db.execSQL("DROP TABLE IF EXISTS Ordine");
+        db.execSQL("DROP TABLE IF EXISTS Domanda");
         onCreate(db);
     }
 
