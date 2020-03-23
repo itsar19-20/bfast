961a294503adeaa9088b782a0704e5a4c5c2db1b
guglielmo-strambini <57755173+guglielmo-strambini@users.noreply.github.com>
2020-03-23 15:15:34 +0100

    Scrittura query per domande

--- Progetto colazioni/Android/AppFattorino/app/src/main/java/com/ifts/bfastfattorino/Business/ScriviDomanda.java
+++ Progetto colazioni/Android/AppFattorino/app/src/main/java/com/ifts/bfastfattorino/Business/ScriviDomanda.java
@@ -2,24 +2,30 @@ package com.ifts.bfastfattorino.Business;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import com.ifts.bfastfattorino.Adapter.DomandaDBAdapter;
 import com.ifts.bfastfattorino.Adapter.FattorinoDBAdapter;
 import com.ifts.bfastfattorino.ModelAPP.Domanda;
 import com.ifts.bfastfattorino.Sessioni.SessionFat;
 
+import retrofit2.http.Query;
+
 public class ScriviDomanda extends AppCompatActivity {
     FattorinoDBAdapter udba = new FattorinoDBAdapter(this);
+    private SessionFat session;
+    private SQLiteDatabase db;
 
     public Domanda registrazione  (String testo)
     {
-        Integer id = SessionFat.getIDfatt();
+        Integer id = session.getIDfatt();
         DomandaDBAdapter ddb = new DomandaDBAdapter();
         Domanda _return = null;
         _return= (Domanda) udba.getUserLogin(id);
 
-        String Ris = ("SELECT d.ID FROM Domanda as d \r\n" +
-                "WHERE d.domanda = \"testo\";")/*.setParameter("testo", testo)*/;
-        if (Ris == null) {
+        Cursor ris = db.rawQuery("SELECT d.ID FROM Domanda as d WHERE d.domanda ="+testo,null);
+
+        if (ris!=null && ris.moveToFirst()) {
             _return = new Domanda();
             _return.setDomanda(testo);
 
