146a2570b5e1fe13df170c5c072717a0c8843071
guglielmo-strambini <57755173+guglielmo-strambini@users.noreply.github.com>
2020-03-23 12:09:47 +0100

    Creazioni sessioni per utente

---
+++ Progetto colazioni/Android/AppUtente/app/src/main/java/com/ifts/bfastutente/Sessioni/Session.java
@@ -0,0 +1,24 @@
+package com.ifts.bfastutente.Sessioni;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class Session {
+
+    private SharedPreferences prefs;
+
+    public Session(Context cntx) {
+        // TODO Auto-generated constructor stub
+        prefs = PreferenceManager.getDefaultSharedPreferences(cntx);
+    }
+
+    public void setMailUt(String mail) {
+        prefs.edit().putString("mail", mail).commit();
+    }
+
+    public String getMailUt() {
+        String mail = prefs.getString("mail","");
+        return mail;
+    }
+}
