9f62a2d9868fcd70169eec934e24ba0d26495c06
guglielmo-strambini <57755173+guglielmo-strambini@users.noreply.github.com>
2020-03-23 15:24:11 +0100

    Lavoro con query per scrittura e lettura domande fattorino

--- Progetto colazioni/Android/AppFattorino/app/src/main/java/com/ifts/bfastfattorino/Business/ScriviRisposta.java
+++ Progetto colazioni/Android/AppFattorino/app/src/main/java/com/ifts/bfastfattorino/Business/ScriviRisposta.java
@@ -1,9 +1,13 @@
 package com.ifts.bfastfattorino.Business;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ifts.bfastfattorino.Adapter.FattorinoDBAdapter;
 import com.ifts.bfastfattorino.Adapter.PossiedeDBAdapter;
+import com.ifts.bfastfattorino.Adapter.RispostaDBAdapter;
 import com.ifts.bfastfattorino.ModelAPP.Domanda;
 import com.ifts.bfastfattorino.ModelAPP.Possiede;
 import com.ifts.bfastfattorino.ModelAPP.Risposta;
@@ -11,40 +15,37 @@ import com.ifts.bfastfattorino.Sessioni.SessionFat;
 
 public class ScriviRisposta extends AppCompatActivity {
     FattorinoDBAdapter udba = new FattorinoDBAdapter(this);
+    private SQLiteDatabase db;
+    private SessionFat session;
 
     public Risposta registrazione(int domanda, String testo) {
-        Integer id = SessionFat.getIDfatt();
+        Integer id = session.getIDfatt();
         Risposta _return = null;
         _return= (Risposta) udba.getUserLogin(id);
+        RispostaDBAdapter rba = new RispostaDBAdapter();
 
         //EntityManager em = JPAUtil.getInstance().getEmf().createEntityManager();
-       String Ris=("SELECT r.ID FROM Risposta as r \r\n" +
-                "WHERE r.Risposta = \"testo\";") /*.setParameter("testo", testo)*/;
-        if (Ris == null) {
+        Cursor ris = db.rawQuery("SELECT r.ID FROM Risposta as r WHERE r.Risposta =" +testo,null);
+        if (ris == null) {
             _return = new Risposta();
             _return.setRisposta(testo);
 
             return _return;
         }else {
-           // _return =em.find(Risposta.class, Ris.getFirstResult());
+           _return = (Risposta) rba.getRisposta(ris.getInt(0));
             return _return;
         }
     }
 
     private void scrivi(int domanda,Risposta _return) {
-        PossiedeDBAdapter pdb= new PossiedeDBAdapter();
-        Possiede p = new Possiede();
-      Domanda d = new Domanda();
-        p.setIdDomanda(domanda);
+         PossiedeDBAdapter pdb= new PossiedeDBAdapter();
+         Possiede p = new Possiede();
+         Domanda d = new Domanda();
+         p.setIdDomanda(domanda);
          p.setIdRisposta(_return.getId());
          pdb.open();
          pdb.addConnessione(d.getDomanda(),_return.getRisposta());
          pdb.close();
-
-
     }
-
-
-
 }
 
