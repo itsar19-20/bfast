a436d06e31c4db415e54396cef9ea5d5e937dcc4
Danidep <57755865+Danidep@users.noreply.github.com>
2020-03-27 17:41:02 +0100

    update maps fattorino

--- Progetto colazioni/Android/AppFattorino/app/src/main/java/com/ifts/bfastfattorino/Business/ViaggioFattorino.java
+++ Progetto colazioni/Android/AppFattorino/app/src/main/java/com/ifts/bfastfattorino/Business/ViaggioFattorino.java
@@ -1,8 +1,19 @@
 package com.ifts.bfastfattorino.Business;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -10,32 +21,72 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.ifts.bfastfattorino.Adapter.PosizioneFattorinoDBAdapter;
 import com.ifts.bfastfattorino.R;
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
 
-import java.util.ArrayList;
-import java.util.List;
 
-    public class ViaggioFattorino extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-        private GoogleMap mMap;
-        PosizioneFattorinoDBAdapter bdb = new PosizioneFattorinoDBAdapter();
-        Marker markerUtente;
+public class ViaggioFattorino extends FragmentActivity implements  OnMapReadyCallback, MapboxMap.OnMapClickListener{
+    // variables for calculating and drawing a route
+    private DirectionsRoute currentRoute;
+    private static final String TAG = "DirectionsActivity";
+    private NavigationMapRoute navigationMapRoute;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_homepage);
-            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.mapView);
-            mapFragment.getMapAsync(this);
+    private GoogleMap mMap;
+    PosizioneFattorinoDBAdapter bdb = new PosizioneFattorinoDBAdapter();
+    Marker markerUtente;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        googleMap.setMyLocationEnabled(true);
+
+        LocationManager locManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria cri = new Criteria();
+        String bestProvider = locManager.getBestProvider(cri, true);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        Location loc = locManager.getLastKnownLocation(bestProvider);
+            String latMy = String.valueOf(loc.getLatitude());
+            String lngMy = String.valueOf(loc.getLongitude());
 
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-            mMap = googleMap;
+            Toast.makeText(this, "Navigation", Toast.LENGTH_SHORT).show();
+            String url = "http://maps.google.com/maps?saddr=" + latMy + ","
+                    + lngMy + "&daddr=" + "18.7703500,19.4534500";
+
+            Intent navigation = new Intent(Intent.ACTION_VIEW);
+            navigation.setData(Uri.parse(url));
+
+            startActivity(navigation);
+
 
             googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
 
@@ -46,6 +97,42 @@ import java.util.List;
             });
         }
 
+    private void getRoute(Point origin, Point destination) {
+        NavigationRoute.builder(this)
+                .accessToken(Mapbox.getAccessToken())
+                .origin(origin)
+                .destination(destination)
+                .build()
+                .getRoute(new Callback<DirectionsResponse>() {
+                    @Override
+                    public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                        // You can get the generic HTTP info about the response
+                        Log.d(TAG, "Response code: " + response.code());
+                        if (response.body() == null) {
+                            Log.e(TAG, "No routes found, make sure you set the right user and access token.");
+                            return;
+                        } else if (response.body().routes().size() < 1) {
+                            Log.e(TAG, "No routes found");
+                            return;
+                        }
+
+                        currentRoute = response.body().routes().get(0);
+
+                        // Draw the route on the map
+                        if (navigationMapRoute != null) {
+                            navigationMapRoute.removeRoute();
+                        } else {
+                            navigationMapRoute = new NavigationMapRoute(null, mapView, mapboxMap, R.style.NavigationMapRoute);
+                        }
+                        navigationMapRoute.addRoute(currentRoute);
+                    }
+
+                    @Override
+                    public void onFailure(Call<DirectionsResponse> call, Throwable throwable) {
+                        Log.e(TAG, "Error: " + throwable.getMessage());
+                    }
+                });
+    }
 
         @Override
         public boolean onMarkerClick(Marker marker) {
@@ -56,4 +143,9 @@ import java.util.List;
         public void onPointerCaptureChanged(boolean hasCapture) {
 
         }
+
+    @Override
+    public boolean onMapClick(@NonNull com.mapbox.mapboxsdk.geometry.LatLng point) {
+        return false;
     }
+}
