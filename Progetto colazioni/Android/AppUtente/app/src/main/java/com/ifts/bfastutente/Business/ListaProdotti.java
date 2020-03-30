12e708a3327cfccd565da89421c98ab4e4a6f471
guglielmo-strambini <57755173+guglielmo-strambini@users.noreply.github.com>
2020-03-30 03:24:56 +0200

    Modifica lista prodotti con l'aggiunta di touch e scroll

--- Progetto colazioni/Android/AppUtente/app/src/main/java/com/ifts/bfastutente/Business/ListaProdotti.java
+++ Progetto colazioni/Android/AppUtente/app/src/main/java/com/ifts/bfastutente/Business/ListaProdotti.java
@@ -1,9 +1,16 @@
 package com.ifts.bfastutente.Business;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.ifts.bfastutente.Adapter.ProdottoDBAdapter;
 import com.ifts.bfastutente.ModelAPP.Prodotto;
 import com.ifts.bfastutente.R;
@@ -11,19 +18,48 @@ import com.ifts.bfastutente.R;
 import java.util.ArrayList;
 import java.util.List;
 
-class ListaProdotti extends AppCompatActivity {
 
+class ListaProdotti extends AppCompatActivity {
+    EditText etAuthor, etTitle, etBody;
     ProdottoDBAdapter pdb = new ProdottoDBAdapter();
+    ConstraintLayout constraintLayout;
+    FloatingActionButton seleziona;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
+        setContentView(R.layout.activity_registrazione);//schermata di lista
+        setTooblar();
+        EditText etNome = findViewById(R.id.ETnome);
+        pdb.open();
         List<String> prodotto = new ArrayList<>();
         prodotto = pdb.getIdProdotto();
         for (int i = 0; i < prodotto.size(); i ++) {
             Prodotto pr = null;
             pr.setNome(prodotto.get(i));
+            etNome.setText(prodotto.get(i));
         }
+        seleziona = findViewById(R.id.BtnLogin);//da vedere in base alla view
+        seleziona.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toHome = new Intent(ListaProdotti.this, MainActivity.class);//da aggiungere activity per carello
+                startActivity(toHome);
+            }
+        });
+    }
+
+
+
+    public void setTooblar() {
+        Toolbar toolbar = findViewById(R.id.toolbar_createActivity);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    protected void onDestroy() {
+        pdb.close();
+        super.onDestroy();
     }
 }
